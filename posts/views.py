3bcddab30c4e4fcfb13873676addb3a3c8d09fea
Dickson Yankey <yankeydickson1@gmail.com>
2021-10-30 15:38:17 +0000

    Message board webpage

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def home(request):
+    post = Post.objects.all()
+    context = {
+        "post": post 
+    }
+    return render(request, 'posts/home.html',context)
